8650b0161506885c29ed84622bea83d56726c0c6
gabriel2m <gabriel2m.contact@gmail.com>
2024-06-25 11:16:01 -0300

    build: config api routing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ForceJsonMiddleware;
 use App\Http\Middleware\LogMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -7,12 +8,14 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        apiPrefix: '',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(LogMiddleware::class);
+        $middleware->append(ForceJsonMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
