d63df7bc04aef1343a0aa07e774635362d18433d
gabriel2m <gabriel2m.contact@gmail.com>
2024-06-28 00:50:29 -0300

    fix: https on prod

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies('*');
         $middleware->append(LogMiddleware::class);
         $middleware->append(ForceJsonMiddleware::class);
     })
