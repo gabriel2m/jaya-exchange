ed38a1d8c8f234dc23b6655fe69708eed4d8c8c3
gabriel2m <gabriel2m.contact@gmail.com>
2024-06-27 22:41:11 -0300

    test: improve latest rates mock

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,8 @@
 
 namespace Tests;
 
-use App\Contracts\Services\ExchangeService;
-use App\Services\ExchangeRatesApiService;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Http;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -16,17 +15,15 @@ abstract class TestCase extends BaseTestCase
 
     protected function mockExchangeRatesApiLatestRates()
     {
-        $this->instance(
-            ExchangeService::class,
-            mock(ExchangeRatesApiService::class)
-                ->makePartial()
-                ->shouldAllowMockingProtectedMethods()
-                ->allows([
-                    'latestRates' => [
-                        'USD' => 1.071593,
-                        'BRL' => 5.841356,
-                    ],
-                ])
-        );
+        Http::preventStrayRequests();
+        Http::fake([
+            str(config('exchange.exchanges.exchange_rates_api.base_url'))->finish('/')->append('latest*')->toString() => fn () => Http::response([
+                'success' => true,
+                'rates' => [
+                    'USD' => 1.071593,
+                    'BRL' => 5.841356,
+                ],
+            ]),
+        ]);
     }
 }
